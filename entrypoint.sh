a97a9d7607b357d22d9c419c861df481976a0ad5
Dario B <feddar@users.noreply.github.com>
2019-08-02 18:03:54 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,11 @@ set -e
 [ "$DEBUG" == 'true' ] && set -x
 
 LOGFILE=/var/log/dovecot/dovecot.log
-mkdir -p /var/log/dovecot/ /tmp/dovecot
+mkdir -p /var/log/dovecot/ /tmp/dovecot /data/dovecot/Maildir
 if [ ! -f $LOGFILE ]; then
   touch $LOGFILE
 fi
-chmod 777 /var/log/dovecot /tmp/dovecot
+chmod 777 /var/log/dovecot /tmp/dovecot /data/dovecot/Maildir
 chmod 666 $LOGFILE
 
 exec tail -f $LOGFILE &
