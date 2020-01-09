74cf73138200ce35a883d60837f3049db19d731a
Dario B <feddar@users.noreply.github.com>
2020-01-09 20:05:24 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,40 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
+DEBUG="${DEBUG:-}"
 [ "$DEBUG" == 'true' ] && set -x
 
-LOGFILE=/var/log/dovecot/dovecot.log
-mkdir -p /var/log/dovecot/ /tmp/dovecot /data/dovecot/Maildir
+LOGDIR="/data/logs"
+[ ! -d "${LOGDIR}" ] && mkdir -p $LOGDIR
+LOGFILE="${LOGDIR}/dovecot.log"
 if [ ! -f $LOGFILE ]; then
   touch $LOGFILE
 fi
-chmod 777 /var/log/dovecot /tmp/dovecot /data/dovecot/Maildir
 chmod 666 $LOGFILE
+HOMEDIRS="${HOMEDIRS:-/data/home}"
+[ ! -d "${HOMEDIRS}" ] && mkdir -p $HOMEDIRS
+chown 5000:5000 $HOMEDIRS
+chmod 775 $HOMEDIRS
 
-exec tail -f $LOGFILE &
+if [ ! -f /etc/dovecot/fullchain.pem ]; then
+  cp /etc/ssl/dovecot/server.pem /etc/dovecot/fullchain.pem
+  cp /etc/ssl/dovecot/server.key /etc/dovecot/privkey.pem 
+fi
+
+if [ -f /servername_cert ]; then
+  servername_cert="$(grep "^[[:alnum:]]" /servername_cert|head -n1|tr -d " ")"
+  if [ -n "$servername_cert" ]; then
+    sed -i "s/^ssl_cert.*$/ssl_cert = <\/data\/certs\/live\/$servername_cert\/fullchain\.pem/" /etc/dovecot/dovecot.conf
+    sed -i "s/^ssl_key.*$/ssl_key = <\/data\/certs\/live\/$servername_cert\/privkey.pem/" /etc/dovecot/dovecot.conf
+  fi
+fi
+
+COMMONDIR="${COMMONDIR:-/data/common}"
+[ ! -d "${COMMONDIR}" ] && mkdir -p $COMMONDIR
+if [ ! -f "${COMMONDIR}/dh-dovecot.pem" ]; then
+  openssl dhparam 2048 > "${COMMONDIR}/dh-dovecot.pem"
+fi
+
+exec tail -f "$LOGFILE" &
 exec "$@"
